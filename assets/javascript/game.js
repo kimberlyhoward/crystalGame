f62876a0f42b4928edfd0bc072d00c8b4bcd4dad
Kimberly Howard <Kimberly Howard>
2018-02-09 21:02:35 -0700

    javescript and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+// Push button to start game or new game.
+
+// Computer choses a random number between 19 - 120.
+var computerNumber = Math.floor((Math.random() * 120) + 19);
+console.log (computerNumber);
+
+// Crystals are made clickable.
+$("#blue").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+// Crystals are given a random number between 1 - 12.
+
+
+// Click on crystal and and the result to number.
+
+
+// If number less, then display in bottom box.
+
+
+// If number more, then lose.
+
+
+// if numbers equals, then win.
+
+// Display wins.
+
+
+// Display losses.
